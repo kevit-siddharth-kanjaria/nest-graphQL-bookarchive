75e05ef30e7bbb6bb1fb152afec48277a03c3688
Siddharth Kanjaria <siddharth.kanjaria@kevit.io>
2024-02-14 11:29:57 +0530

    approach change to code first

--- src/components/author/author.module.ts
+++ src/components/author/author.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { AuthorService } from './author.service';
-import { AuthorResolver } from './author.resolver';
 import { MongooseModule } from '@nestjs/mongoose';
 import { AuthorSchema } from './author.schema';
 
 @Module({
     imports: [MongooseModule.forFeature([{ name: 'Author', schema: AuthorSchema }])],
-    providers: [AuthorService, AuthorResolver],
+    providers: [],
 })
 export class AuthorModule {}
